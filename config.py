a1a355bb0e98d0829d44cfe4980075122ce9e48b
edogola4 <edogola4@gmail.com>
2025-03-31 14:33:10 +0300

    updated

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from sqlalchemy import create_engine
 
 BASE_DIR = Path(__file__).resolve().parent
 DATABASE_URI = f"sqlite:///{BASE_DIR}/inventory.db"
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URI, echo=True)
